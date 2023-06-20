70f432659bd32ae187c1f1c8eeb1a0f3a217005c
Irshal Firdeansyah <irshalfir@gmail.com>
2023-06-20 22:05:59 +0700

    implement page data with paging 2

--- app/src/main/java/com/dicoding/mystudentdata/StudentRepository.kt
+++ app/src/main/java/com/dicoding/mystudentdata/StudentRepository.kt
@@ -1,6 +1,8 @@
 package com.dicoding.mystudentdata
 
 import androidx.lifecycle.LiveData
+import androidx.paging.LivePagedListBuilder
+import androidx.paging.PagedList
 import com.dicoding.mystudentdata.database.Student
 import com.dicoding.mystudentdata.database.StudentAndUniversity
 import com.dicoding.mystudentdata.database.StudentDao
@@ -12,9 +14,17 @@ import com.dicoding.mystudentdata.helper.SortUtils
 
 class StudentRepository(private val studentDao: StudentDao) {
 
-    fun getAllStudent(sortType: SortType): LiveData<List<Student>> {
+    fun getAllStudent(sortType: SortType): LiveData<PagedList<Student>> {
         val query = SortUtils.getSortedQuery(sortType)
-        return studentDao.getAllStudent(query)
+        val student = studentDao.getAllStudent(query)
+
+        val config = PagedList.Config.Builder()
+            .setEnablePlaceholders(true)
+            .setInitialLoadSizeHint(30)
+            .setPageSize(10)
+            .build()
+
+        return LivePagedListBuilder(student, config).build()
     }
 
     fun getAllStudentAndUniversity(): LiveData<List<StudentAndUniversity>> = studentDao.getAllStudentAndUniversity()
